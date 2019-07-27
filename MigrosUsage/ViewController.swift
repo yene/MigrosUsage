c5e4bfba6e658c68a68c28e21d690c29b4461fbb
Yannick Weiss <contact@yannickweiss.com>
2019-07-27 19:28:42 +0200

    Initial Commit

---
+++ MigrosUsage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MigrosUsage
+//
+//  Created by Yannick on 27.07.19.
+//  Copyright © 2019 yannick.dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
